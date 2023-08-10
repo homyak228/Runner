00b432ed454d33c10d992dc6dcb6d5af71a79296
Игорь <gre.igroman228@gmail.com>
2023-08-10 20:17:50 +0300

    Modiffed script ad added graphics

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ pygame.display.set_caption("Runner")
 pygame.display.set_icon(pygame.image.load("images/GUI/icon.png").convert_alpha())
 
 heart = pygame.transform.scale(pygame.image.load("images/GUI/Heart.png").convert_alpha(), (50, 50))
+player = [pygame.transform.scale(pygame.image.load("images/Player/Player1.png").convert_alpha(), (70, 70)),
+          pygame.transform.scale(pygame.image.load("images/Player/Player2.png").convert_alpha(), (70, 70))]
+BG = pygame.image.load("images/BG.png").convert()
 
 lives = 3
+animation_frame = 0
+BG_X = 0
+
+clock = pygame.time.Clock()
 
 def draw_hearts(lives):
     for x in range(lives):
@@ -19,7 +26,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    window.blit(BG, (BG_X + 600, 0))
+    window.blit(BG, (BG_X, 0))
+    window.blit(player[animation_frame], (100, 330))
     draw_hearts(lives)
     pygame.display.update()
+
+    clock.tick(10)
+
+    BG_X += -5
+    animation_frame += 1
+    if animation_frame > 1:
+        animation_frame = 0
+    if BG_X <= -600:
+        BG_X = 0
 
 pygame.quit()
